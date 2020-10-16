50ee230a27ffff6e159d21683c99e1e79b9b7c20
Angel Baro <angelbaro44@gmail.com>
2020-10-16 19:32:10 -0400

    added some test examples

--- index.js
+++ index.js
@@ -1,1 +1,9 @@
 let isPangram = (string) =>[...'qwertyuiopasdfghjklzxcvbnm'].every(x => string.toLowerCase().indexOf(x) !== -1);
+ 
+//some test examples
+console.log(isPangram("Cwm fjord bank glyphs vext quiz")); //ture
+console.log(isPangram("Pack my box with five dozen liquor jugs.")); //ture
+console.log(isPangram("ABCD45EFGH,IJK,LMNOPQR56STUVW3XYZ")); //true
+console.log(isPangram("A pangram is a sentence that contains every single letter of the alphabet at least once.")); //false
+console.log(isPangram("This isn't a pangram!")); //false
+console.log(isPangram("aaaaaaaaaaaaaaaaaaaaaaaaaa")); //false
